cb777cd9e882a3b9b51236428ae7dd9057217140
j.taneichi <j.tane3ma@gmail.com>
2016-01-14 17:40:53 +0900

    create api service

---
+++ app/api.service.ts
@@ -0,0 +1,52 @@
+import {Component} from 'angular2/core';
+import {Injectable} from 'angular2/core';
+import { CORE_DIRECTIVES } from 'angular2/common';
+import {HTTP_PROVIDERS, Http, Headers } from 'angular2/http';
+@Component({
+  directives: [CORE_DIRECTIVES],
+  providers: [HTTP_PROVIDERS]
+})
+@Injectable()
+export class ApiService {
+    constructor(private _http: Http) { }
+    callGetAnonymousApi(url: string, params?: any) {
+        if (params) {
+            this._callGetApi('Anonymous', url, params);
+        } else {
+            this._callGetApi('Anonymous', url);
+        }
+    }
+
+    callGetSecuredApi() {
+        this._callGetApi('Secured', 'http://localhost:3001/api/protected/random-quote');
+    }
+
+    _callGetApi(type: string, url: string, params?: any) {
+        console.log(params)
+        if (params) {
+            log = Object.keys(params).map(function(value, index) {
+                return params[value]
+            });
+            console.log("kiteru");
+            console.log(log);
+        }
+        this.response = null;
+        if (type === 'Anonymous') {
+            // For non-protected routes, just use Http
+            this._http.get(url)
+                .subscribe(
+                data => console.log(data),
+                err => console.log(err),
+                () => console.log('Random Quote Complete')
+                );
+        }
+        // if (type === 'Secured') {
+        //   // For protected routes, use AuthHttp
+        //   this._authHttp.get(url)
+        //     .subscribe(
+        //     response => this.response = response.text(),
+        //     error => this.response = error.text()
+        //   );
+        // }
+    }
+}
--- app/dashboard/dashboard.component.ts
+++ app/dashboard/dashboard.component.ts
@@ -1,18 +1,17 @@
 import {Component} from 'angular2/core';
-import { CORE_DIRECTIVES } from 'angular2/common';
-import {HTTP_PROVIDERS, Http, Headers } from 'angular2/http';
 import { Router } from 'angular2/router';
 import {HeaderComponent} from 'app/layouts/header/header.component';
 import {SidebarComponent} from 'app/layouts/sidebar/sidebar.component';
 import {Hero} from 'app/hero';
 import {HeroDetailComponent} from 'app/hero-detail.component';
 import {HeroService} from 'app/hero.service';
+import {ApiService} from 'app/api.service';
 @Component({
   selector: 'dashboard',
   templateUrl: 'app/dashboard/dashboard.component.html',
   styleUrls: ['app/dashboard/dashboard.component.css'],
-  directives: [CORE_DIRECTIVES, HeaderComponent, SidebarComponent, HeroDetailComponent],
-  providers: [HeroService, HTTP_PROVIDERS]
+  directives: [HeaderComponent, SidebarComponent, HeroDetailComponent],
+  providers: [HeroService,ApiService]
 })
 
 export class DashboardComponent implements OnInit {
@@ -25,8 +24,8 @@ export class DashboardComponent implements OnInit {
   public heroes: Hero[];
   public selectedHero: Hero;
 
-  constructor(private _heroService: HeroService, private _router: Router, private _http: Http) {
-      console.log(this.callGetAnonymousApi('http://api.stoq.jp/api/v1/courses', { hoge: "wa", huga: "ho" }))
+  constructor(private _heroService: HeroService, private _router: Router, private _http: ApiService) {
+      console.log(_http.callGetAnonymousApi('http://api.stoq.jp/api/v1/courses', { hoge: "wa", huga: "ho" }))
     console.log(this.response)
   }
 
@@ -44,46 +43,5 @@ export class DashboardComponent implements OnInit {
   }
 
   onSelect(hero: Hero) { this.selectedHero = hero; }
-
-  callGetAnonymousApi(url: string, params?: any) {
-    if(params) {
-      this._callGetApi('Anonymous', url, params);
-    }else {
-      this._callGetApi('Anonymous', url);
-    }
-  }
-
-  callGetSecuredApi() {
-    this._callGetApi('Secured', 'http://localhost:3001/api/protected/random-quote');
-  }
-
-  _callGetApi(type: string, url: string, params?: any) {
-    console.log(params)
-    if (params) {
-      log = Object.keys(params).map(function(value, index) {
-          return params[value]
-      });
-      console.log("kiteru");
-      console.log(log);
-    }
-    this.response = null;
-    if (type === 'Anonymous') {
-      // For non-protected routes, just use Http
-      this._http.get(url)
-        .subscribe(
-          data => console.log(data),
-          err => console.log(err),
-          () => console.log('Random Quote Complete')
-      );
-    }
-    // if (type === 'Secured') {
-    //   // For protected routes, use AuthHttp
-    //   this._authHttp.get(url)
-    //     .subscribe(
-    //     response => this.response = response.text(),
-    //     error => this.response = error.text()
-    //   );
-    // }
-  }
 }
 
