19627c7585d311eafa4696252e0038401e873daa
j.taneichi <j.tane3ma@gmail.com>
2016-01-15 15:05:11 +0900

    remove

--- app/common/headers.ts
+++ app/common/headers.ts
@@ -3,5 +3,4 @@ import { Headers } from 'angular2/http';
 export const contentHeaders = new Headers();
 contentHeaders.append('Accept', 'application/json');
 contentHeaders.append('Content-Type', 'application/json');
-contentHeaders.append('Access-Token', localStorage.getItem('Access-Token'));
 
