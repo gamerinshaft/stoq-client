5b75714036f01044ac29c6240da4738bb9f728c7
j.taneichi <j.tane3ma@gmail.com>
2016-01-15 15:05:35 +0900

    add setHEader

--- app/api.service.ts
+++ app/api.service.ts
@@ -50,6 +50,7 @@ export class ApiService {
     }
 
     _callGetApi(type: string, url: string, params?: any) {
+        this._setHeader();
         url = this.api_path + this._urlWithQuery(url, params);
         if (type === 'Anonymous') {
             // For non-protected routes, just use Http
@@ -69,14 +70,15 @@ export class ApiService {
         // }
     }
     _callPostApi(type: string, url: string, params?: any) {
-        let body = JSON.stringify(params)
+        this._setHeader();
+        let body = JSON.stringify(params);
         url = this.api_path + url
         if (type === 'Anonymous') {
             return this._http.post(url, body, { headers: contentHeaders });
         }
     }
     _callDeleteApi(type: string, url: string, params?: any) {
-        console.log(contentHeaders);
+        this._setHeader();
         url = this.api_path + this._urlWithQuery(url, params);
         if (type === 'Anonymous') {
             return this._http.delete(url, { headers: contentHeaders });
@@ -94,5 +96,11 @@ export class ApiService {
             });
         }
         return url;
+    }
+
+    _setHeader() {
+        contentHeaders.set('Access-Token', localStorage.getItem('Access-Token'));
+        contentHeaders.set('Client', localStorage.getItem('Client'));
+        contentHeaders.set('Uid', localStorage.getItem('Uid'));
     }
 }
