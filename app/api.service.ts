e933a0398253a1d9b6345a7bef893392a640b78f
j.taneichi <j.tane3ma@gmail.com>
2016-01-15 09:29:23 +0900

    update api service

--- app/api.service.ts
+++ app/api.service.ts
@@ -2,6 +2,7 @@ import {Component} from 'angular2/core';
 import {Injectable} from 'angular2/core';
 import { CORE_DIRECTIVES } from 'angular2/common';
 import {HTTP_PROVIDERS, Http, Headers } from 'angular2/http';
+import { contentHeaders } from './common/headers';
 
 @Component({
   directives: [CORE_DIRECTIVES],
@@ -11,95 +12,76 @@ import {HTTP_PROVIDERS, Http, Headers } from 'angular2/http';
 @Injectable()
 
 export class ApiService {
-    response: any;
     public api_path = 'http://api.stoq.jp/api/v1';
+    private status: number;
+    private body: string;
     constructor(private _http: Http) {}
 
 
     // ユーザー登録 email, password, password_confirmation
     postSignup(params: any) {
-        this._callPostApi('Anonymous', '/auth', params);
+        return this._callPostApi('Anonymous', '/auth', params);
     }
 
     // ログイン email, password
     postLogin(params: any) {
-        this._callPostApi('Anonymous', '/auth/sign_in', params);
+        return this._callPostApi('Anonymous', '/auth/sign_in', params);
     }
 
     deleteLogout() {
-        this._callDeleteApi('Anonymous', '/auth/sign_out');
+        return this._callDeleteApi('Anonymous', '/auth/sign_out');
     }
 
     // コース一覧を取得 引数:なし
     getCourses() {
-        this._callGetApi('Anonymous', '/courses');
+        return this._callGetApi('Anonymous', '/courses');
     }
 
     // コースを作成, title, description, allow_training, allow_test, allow_show_answer, is_private, questions_attributes, text, hint, questiontype, answer_attributes, value, is_dummy
     postCreateCourse(params: any) {
-        this._callPostApi('Anonymous', '/courses/new', params);
+        return this._callPostApi('Anonymous', '/courses/new', params);
     }
 
     postEditCourse(id: string, params: any) {
-        this._callPostApi('Anonymous', '/courses/' + id + 'edit', params);
+        return this._callPostApi('Anonymous', '/courses/' + id + 'edit', params);
     }
 
     getCourseDetail(id: string) {
-        this._callGetApi('Anonymous', '/courses/' + id);
+        return this._callGetApi('Anonymous', '/courses/' + id);
     }
 
-    _callGetApi(type: string, url: string, params?: any) {
-        this.response = null;
+    _callGetApi(type: string, url: string, params?: any)  {
         url = this.api_path + this._urlWithQuery(url, params);
-        console.log(url)
         if (type === 'Anonymous') {
             // For non-protected routes, just use Http
-            this._http.get(url)
-                .subscribe(
-                data => this.response = data,
-                err => {
-                    this.response = err; console.log(err);
-                },
-                () => console.log('Random Quote Complete')
-                );
+            return this._http.get(url)
+                // .subscribe(res => {
+                //     this.status = res.status;
+                //    this .body = res.json();
+                // });
         }
-        return "hoge";
+        console.log(this.status)
+        console.log(this.body)
         // if (type === 'Secured') {
         //   // For protected routes, use AuthHttp
         //   this._authHttp.get(url)
         //     .subscribe(
-        //     response => this.response = response.text(),
-        //     error => this.response = error.text()
+        //     response => this.result = response.text(),
+        //     error => this.result = error.text()
         //   );
         // }
     }
     _callPostApi(type: string, url: string, params?: any) {
-        this.response = null;
-        url = this.api_path + this._urlWithQuery(url, params);
+        url = this.api_path + url
         if (type === 'Anonymous') {
-            this._http.post(url)
-                .subscribe(
-                    data => this.response = data,
-                    err => this.response = err,
-                    () => console.log('Random Quote Complete')
-                );
+            return this._http.post(url, body, { headers: contentHeaders });
         }
-        console.log(this.response)
-        return this.response;
     }
     _callDeleteApi(type: string, url: string, params?: any) {
-        this.response = null;
         url = this.api_path + this._urlWithQuery(url, params);
         if (type === 'Anonymous') {
-            this._http.delete(url)
-                .subscribe(
-                data => this.response = data,
-                err => this.response = err,
-                () => console.log('Random Quote Complete')
-                );
+            return this._http.delete(url);
         }
-        console.log(this.response)
-        return this.response;
     }
 
     _urlWithQuery(url: string, params?: any) {
