452a506066cb2740fab890b0f7c3d65cfab04010
j.taneichi <j.tane3ma@gmail.com>
2016-01-15 09:29:35 +0900

    update app component

--- app/app.component.ts
+++ app/app.component.ts
@@ -3,17 +3,18 @@ import {RouteConfig, ROUTER_DIRECTIVES, Router} from 'angular2/router';
 import {DashboardComponent} from './dashboard/dashboard.component';
 import {LoginComponent} from './login/login.component';
 import {SignupComponent} from './signup/signup.component';
-
+import {ApiService} from './api.service';
 
 @Component({
     selector: 'my-app',
     templateUrl: 'app/app.component.html',
     styleUrls: ['app/app.component.scss'],
-    directives: [ROUTER_DIRECTIVES, LoginComponent, SignupComponent, DashboardComponent]
+    directives: [ROUTER_DIRECTIVES, LoginComponent, SignupComponent, DashboardComponent],
+    providers: [ApiService]
 })
 
 @RouteConfig([
-    { path: '/dashboard', name: 'Dashboard', component: DashboardComponent, useAsDefault: true },
+    { path: '/dashboard', name: 'Dashboard', component: DashboardComponent, useAsDefault: false },
     { path: '/login', name: 'Login', component: LoginComponent, useAsDefault: false },
     { path: '/signup', name: 'Signup', component: SignupComponent, useAsDefault: false }
   // { path: '/detail/:id', name: 'HeroDetail', component: HeroDetailComponent }
@@ -25,13 +26,18 @@ export class AppComponent implements OnInit {
   // public heroes: Hero[];
   // public selectedHero: Hero;
 
-  constructor(private _router: Router) { }
+  constructor(private _router: Router, private _api: ApiService) { }
 
   // getHeroes() {
   //     this._heroService.getHeroes().then(heroes => this.heroes = heroes);
   // }
 
   ngOnInit() {
+    this._api.getCourses().subscribe(
+      data => this._router.navigate(['Dashboard', {}]),
+      err => this._router.navigate(['Login', {}]),
+      () => console.log('Random Quote Complete')
+    );
     // this.getHeroes();
     // if(!localStorage.getItem("auth_token")){
     //   console.log("nothing auth_token");
