adbd38935f4fd849eaa585d857a8f61dbeac206c
j.taneichi <j.tane3ma@gmail.com>
2016-01-16 15:08:15 +0900

    remove legacy

--- app/dashboard/dashboard.component.ts
+++ app/dashboard/dashboard.component.ts
@@ -2,17 +2,14 @@ import {Component, OnInit} from 'angular2/core';
 import { Router } from 'angular2/router';
 import {HeaderComponent} from '../layouts/header/header.component';
 import {SidebarComponent} from '../layouts/sidebar/sidebar.component';
-import {Hero} from '../hero';
-import {HeroDetailComponent} from '../hero-detail.component';
-import {HeroService} from '../hero.service';
 import {ApiService} from '../api.service';
 
 @Component({
   selector: 'dashboard',
   templateUrl: 'app/dashboard/dashboard.component.html',
   styleUrls: ['app/dashboard/dashboard.component.css'],
-  directives: [HeaderComponent, SidebarComponent, HeroDetailComponent],
-  providers: [HeroService,ApiService]
+  directives: [HeaderComponent, SidebarComponent],
+  providers: [ApiService]
 })
 
 export class DashboardComponent implements OnInit {
@@ -22,27 +19,17 @@ export class DashboardComponent implements OnInit {
   public api: string;
   public title = 'コース一覧';
   public modal = 'signup';
-  public heroes: Hero[];
-  public selectedHero: Hero;
 
-  constructor(private _heroService: HeroService, private _router: Router, private _api: ApiService) {
+  constructor(private _router: Router, private _api: ApiService) {
       // console.log(_api.getCourses());
   }
 
-
-  getHeroes() {
-      this._heroService.getHeroes().then(heroes => this.heroes = heroes);
-  }
-
   ngOnInit() {
-    this.getHeroes();
     this._api.getCourses().subscribe(
         data => console.log(data),
         err => console.log(err),
         () => console.log("getCourses")
     );
   }
-
-  onSelect(hero: Hero) { this.selectedHero = hero; }
 }
 
