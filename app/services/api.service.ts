6d34f4989949da0978502e7f2774e09a7177a2c5
j.taneichi <j.tane3ma@gmail.com>
2016-01-31 00:31:32 +0900

    fix err:response:error

--- app/services/api.service.ts
+++ app/services/api.service.ts
@@ -28,31 +28,31 @@ export class ApiService {
     }
 
     deleteLogout() {
-        return this._callDeleteApi('Anonymous', '/auth/sign_out');
+        return this._callDeleteApi('Secured', '/auth/sign_out');
     }
 
     // コース一覧を取得 引数:なし
     getCourses() {
-        return this._callGetApi('Anonymous', '/courses');
+        return this._callGetApi('Secured', '/courses');
     }
 
     // コースを作成, title, description, allow_training, allow_test, allow_show_answer, is_private, questions_attributes, text, hint, questiontype, answer_attributes, value, is_dummy
     postCreateCourse(params: any) {
-        return this._callPostApi('Anonymous', '/courses/new', params);
+        return this._callPostApi('Secured', '/courses/new', params);
     }
 
     postEditCourse(id: string, params: any) {
-        return this._callPostApi('Anonymous', '/courses/' + id + '/edit', params);
+        return this._callPostApi('Secured', '/courses/' + id + '/edit', params);
     }
 
     getCourseDetail(id: string) {
-        return this._callGetApi('Anonymous', '/courses/' + id);
+        return this._callGetApi('Secured', '/courses/' + id);
     }
 
     _callGetApi(type: string, url: string, params?: any) {
-        this._setHeader();
         url = this.api_path + this._urlWithQuery(url, params);
         if (type === 'Anonymous') {
+            this._setHeader();
             // For non-protected routes, just use Http
             console.log(this.headers);
             return this._http.get(url, { headers: this.headers });
@@ -60,30 +60,40 @@ export class ApiService {
                 //     this.status = res.status;
                 //    this .body = res.json();
                 // });
+        } else if (type === 'Secured') {
+            this._setHeader(true);
+            return this._http.get(url, { headers: this.headers });
         }
-        // if (type === 'Secured') {
-        //   // For protected routes, use AuthHttp
-        //   this._authHttp.get(url)
-        //     .subscribe(
-        //     response => this.result = response.text(),
-        //     error => this.result = error.text()
-        //   );
-        // }
     }
+    //     if (type === 'Secured') {
+    //       // For protected routes, use AuthHttp
+    //       this._authHttp.get(url)
+    //         .subscribe(
+    //         response => this.result = response.text(),
+    //         error => this.result = error.text()
+    //       );
+    //     }
+    // }
     _callPostApi(type: string, url: string, params?: any) {
-        // this._setHeader();
-        this._setHeader();
         params = JSON.stringify(params);
         console.log(params)
         url = this.api_path + url
         if (type === 'Anonymous') {
+            this._setHeader();
+            return this._http.post(url, params, { headers: this.headers });
+        } else if (type === 'Secured') {
+            this._setHeader(true);
             return this._http.post(url, params, { headers: this.headers });
         }
+
     }
     _callDeleteApi(type: string, url: string, params?: any) {
-        this._setHeader();
         url = this.api_path + this._urlWithQuery(url, params);
         if (type === 'Anonymous') {
+            this._setHeader();
+            return this._http.delete(url, { headers: this.headers });
+        } else if (type === 'Secured') {
+            this._setHeader(true);
             return this._http.delete(url, { headers: this.headers });
         }
     }
@@ -101,12 +111,13 @@ export class ApiService {
         return url;
     }
 
-    _setHeader(method?: string) {
+    _setHeader(auth?: boolean) {
         this.headers = new Headers();
         this.headers.append('Accept', 'application/json');
         this.headers.append('Content-Type', 'application/json; charset=utf-8');
         this.headers.append('Cache-Control', 'no-chache');
-        if(localStorage.getItem('Access-Token') && localStorage.getItem('Client') && localStorage.getItem('Uid')){
+        console.log("naindayo")
+        if(auth && localStorage.getItem('Access-Token') && localStorage.getItem('Client') && localStorage.getItem('Uid')){
             console.log("set_header");
             this.headers.append('Access-Token', localStorage.getItem('Access-Token'));
             this.headers.append('Client', localStorage.getItem('Client'));
